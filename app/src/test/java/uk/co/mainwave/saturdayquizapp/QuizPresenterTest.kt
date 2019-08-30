0250f21445bb9fa680ea255dd0c45a6b429e75fe
Mark Whitaker <mark@mainwave.co.uk>
2019-08-30 17:19:45 +0100

    Switched from Mockito to Mockk

--- app/src/test/java/uk/co/mainwave/saturdayquizapp/QuizPresenterTest.kt
+++ app/src/test/java/uk/co/mainwave/saturdayquizapp/QuizPresenterTest.kt
@@ -1,9 +1,9 @@
 package uk.co.mainwave.saturdayquizapp
 
-import com.nhaarman.mockitokotlin2.inOrder
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
+import io.mockk.excludeRecords
+import io.mockk.mockk
+import io.mockk.verifyAll
+import io.mockk.verifyOrder
 import org.junit.Test
 import uk.co.mainwave.saturdayquizapp.model.Question
 import uk.co.mainwave.saturdayquizapp.model.QuestionType
@@ -12,8 +12,8 @@ import java.util.Date
 
 class QuizPresenterTest {
     // Mocks
-    private val mockRepository: QuizRepository = mock()
-    private val mockView: QuizPresenter.View = mock()
+    private val mockRepository = mockk<QuizRepository>(relaxUnitFun = true)
+    private val mockView = mockk<QuizPresenter.View>(relaxUnitFun = true)
 
     private val presenter = QuizPresenter(
         mockRepository
@@ -25,13 +25,16 @@ class QuizPresenterTest {
         // When
         presenter.onViewCreated(mockView)
         // Then
-        verify(mockView).showLoading()
-        verify(mockRepository).loadQuiz(presenter)
+        verifyAll {
+            mockView.showLoading()
+            mockRepository.loadQuiz(presenter)
+        }
     }
 
     @Test
     fun `GIVEN view created WHEN onQuizLoaded() THEN loading view is hidden and questions title is shown`() {
         // Given
+        excludeRecords { mockView.showLoading() }
         val date = Date()
         val quiz = Quiz(
             "id",
@@ -43,98 +46,136 @@ class QuizPresenterTest {
         // When
         presenter.onQuizLoaded(quiz)
         // Then
-        verify(mockView).hideLoading()
-        verify(mockView).showQuestionsTitle(date)
+        verifyAll {
+            mockView.hideLoading()
+            mockView.showQuestionsTitle(date)
+        }
     }
 
     @Test
     fun `GIVEN view created WHEN onQuizLoadFailed() THEN view quits`() {
         // Given
+        excludeRecords { mockView.showLoading() }
         presenter.onViewCreated(mockView)
         // When
         presenter.onQuizLoadFailed()
         // Then
-        verify(mockView).quit()
+        verifyAll {
+            mockView.quit()
+        }
     }
 
     @Test
     fun `GIVEN view created and quiz loaded WHEN onNext() THEN next scene is shown`() {
         // Given
-        val quiz = Quiz(
-            "id",
-            Date(),
-            "title",
-            listOf(
-                Question(
-                    1,
-                    QuestionType.NORMAL,
-                    "question1",
-                    "answer1"
-                ),
-                Question(
-                    2,
-                    QuestionType.WHAT_LINKS,
-                    "question2",
-                    "answer2"
-                )
-            )
-        )
+        excludeRecords {
+            mockView.showLoading()
+            mockView.hideLoading()
+        }
         presenter.onViewCreated(mockView)
-        presenter.onQuizLoaded(quiz)
+        presenter.onQuizLoaded(TEST_QUIZ)
         // When
         repeat(9) {
             presenter.onNext()
         }
         // Then
-        inOrder(
-            mockView
-        ) {
+        verifyOrder {
             // onNext() 1
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(1)
-            verify(mockView).showQuestion("question1", false)
-            verify(mockView).showAnswer("")
+            mockView.hideTitle()
+            mockView.showNumber(1)
+            mockView.showQuestion("question1", false)
+            mockView.showAnswer("")
             // onNext() 2
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(2)
-            verify(mockView).showQuestion("question2", true)
-            verify(mockView).showAnswer("")
+            mockView.hideTitle()
+            mockView.showNumber(2)
+            mockView.showQuestion("question2", true)
+            mockView.showAnswer("")
             // onNext() 3
-            verify(mockView).showAnswersTitle()
+            mockView.showAnswersTitle()
             // onNext() 4
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(1)
-            verify(mockView).showQuestion("question1", false)
-            verify(mockView).showAnswer("")
+            mockView.hideTitle()
+            mockView.showNumber(1)
+            mockView.showQuestion("question1", false)
+            mockView.showAnswer("")
             // onNext() 5
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(1)
-            verify(mockView).showQuestion("question1", false)
-            verify(mockView).showAnswer("answer1")
+            mockView.hideTitle()
+            mockView.showNumber(1)
+            mockView.showQuestion("question1", false)
+            mockView.showAnswer("answer1")
             // onNext() 6
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(2)
-            verify(mockView).showQuestion("question2", true)
-            verify(mockView).showAnswer("")
+            mockView.hideTitle()
+            mockView.showNumber(2)
+            mockView.showQuestion("question2", true)
+            mockView.showAnswer("")
             // onNext() 7
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(2)
-            verify(mockView).showQuestion("question2", true)
-            verify(mockView).showAnswer("answer2")
+            mockView.hideTitle()
+            mockView.showNumber(2)
+            mockView.showQuestion("question2", true)
+            mockView.showAnswer("answer2")
             // onNext() 8
-            verify(mockView).showEndTitle()
+            mockView.showEndTitle()
             // onNext() 9
-            verify(mockView).quit()
+            mockView.quit()
         }
     }
 
     @Test
     fun `GIVEN view created and last question and answer shown WHEN onPrevious() THEN previous scene is shown`() {
         // Given
-        val date = Date()
-        val quiz = Quiz(
+        presenter.onViewCreated(mockView)
+        presenter.onQuizLoaded(TEST_QUIZ)
+        repeat(8) {
+            presenter.onNext()
+        }
+        // When/Then
+        // 1
+        repeat(9) {
+            presenter.onPrevious()
+        }
+        // Then
+        verifyOrder {
+            // onPrevious() 1
+            mockView.hideTitle()
+            mockView.showNumber(2)
+            mockView.showQuestion("question2", true)
+            mockView.showAnswer("answer2")
+            // onPrevious() 2
+            mockView.hideTitle()
+            mockView.showNumber(2)
+            mockView.showQuestion("question2", true)
+            mockView.showAnswer("")
+            // onPrevious() 3
+            mockView.hideTitle()
+            mockView.showNumber(1)
+            mockView.showQuestion("question1", false)
+            mockView.showAnswer("answer1")
+            // onPrevious() 4
+            mockView.hideTitle()
+            mockView.showNumber(1)
+            mockView.showQuestion("question1", false)
+            mockView.showAnswer("")
+            // onPrevious() 5
+            mockView.showAnswersTitle()
+            // onPrevious() 6
+            mockView.hideTitle()
+            mockView.showNumber(2)
+            mockView.showQuestion("question2", true)
+            mockView.showAnswer("")
+            // onPrevious() 7
+            mockView.hideTitle()
+            mockView.showNumber(1)
+            mockView.showQuestion("question1", false)
+            mockView.showAnswer("")
+            // onPrevious() 8 (and 9 is ignored)
+            mockView.showQuestionsTitle(TEST_DATE)
+        }
+    }
+
+    companion object {
+        private val TEST_DATE = Date()
+        private val TEST_QUIZ = Quiz(
             "id",
-            date,
+            TEST_DATE,
             "title",
             listOf(
                 Question(
@@ -151,53 +192,5 @@ class QuizPresenterTest {
                 )
             )
         )
-        presenter.onViewCreated(mockView)
-        presenter.onQuizLoaded(quiz)
-        repeat(8) {
-            presenter.onNext()
-        }
-        // When
-        repeat(9) {
-            presenter.onPrevious()
-        }
-        // Then
-        inOrder(
-            mockView
-        ) {
-            // onPrevious() 1
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(2)
-            verify(mockView).showQuestion("question2", true)
-            verify(mockView).showAnswer("answer2")
-            // onPrevious() 2
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(2)
-            verify(mockView).showQuestion("question2", true)
-            verify(mockView).showAnswer("")
-            // onPrevious() 3
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(1)
-            verify(mockView).showQuestion("question1", false)
-            verify(mockView).showAnswer("answer1")
-            // onPrevious() 4
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(1)
-            verify(mockView).showQuestion("question1", false)
-            verify(mockView).showAnswer("")
-            // onPrevious() 5
-            verify(mockView).showAnswersTitle()
-            // onPrevious() 6
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(2)
-            verify(mockView).showQuestion("question2", true)
-            verify(mockView).showAnswer("")
-            // onPrevious() 7
-            verify(mockView).hideTitle()
-            verify(mockView).showNumber(1)
-            verify(mockView).showQuestion("question1", false)
-            verify(mockView).showAnswer("")
-            // onPrevious() 8 (and 9 is ignored)
-            verify(mockView, times(1)).showQuestionsTitle(date)
-        }
     }
 }
