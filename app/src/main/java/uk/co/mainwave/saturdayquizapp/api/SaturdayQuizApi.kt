daea2ac067198f36ba0891b165e806d1dec9ec3f
Mark Whitaker <mark@mainwave.co.uk>
2019-09-22 19:25:47 +0100

    Switched to new .NET Core backend API (#5)

--- app/src/main/java/uk/co/mainwave/saturdayquizapp/api/SaturdayQuizApi.kt
+++ app/src/main/java/uk/co/mainwave/saturdayquizapp/api/SaturdayQuizApi.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 import uk.co.mainwave.saturdayquizapp.model.Quiz
 
 interface SaturdayQuizApi {
-    @GET("/quiz")
+    @GET("quiz")
     fun getLatestQuiz(): Call<Quiz>
 }
