934b9f64cd288029ff89eded2bb9888a058d6ac8
Mark Whitaker <mark@mainwave.co.uk>
2019-11-04 00:37:12 +0000

    Added Retrofit logging

--- app/src/main/java/uk/co/mainwave/saturdayquizapp/di/NetworkModule.kt
+++ app/src/main/java/uk/co/mainwave/saturdayquizapp/di/NetworkModule.kt
@@ -1,6 +1,9 @@
 package uk.co.mainwave.saturdayquizapp.di
 
+import android.util.Log
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Logger
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,10 +16,20 @@ val networkModule = module {
     single { provideRetrofit(get()) }
 }
 
-private fun provideOkHttp(): OkHttpClient = OkHttpClient.Builder()
-    .callTimeout(30, TimeUnit.SECONDS)
-    .readTimeout(30, TimeUnit.SECONDS)
-    .build()
+private fun provideOkHttp(): OkHttpClient {
+    val loggingInterceptor = HttpLoggingInterceptor(object : Logger {
+        override fun log(message: String) {
+            Log.i("HTTP_LOG", message)
+        }
+    })
+    loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+    return OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .callTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+}
 
 private fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
     .baseUrl("https://saturday-quiz.herokuapp.com/api/")
