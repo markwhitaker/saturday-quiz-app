12bf66f978bbbd11153aa1009fa4769e3f661a9e
Mark Whitaker <markw@bitrot.net>
2022-07-24 10:31:32 +0100

    Update Koin

--- app/src/main/java/uk/co/mainwave/saturdayquizapp/di/QuizModule.kt
+++ app/src/main/java/uk/co/mainwave/saturdayquizapp/di/QuizModule.kt
@@ -1,13 +1,14 @@
 package uk.co.mainwave.saturdayquizapp.di
 
-import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 import uk.co.mainwave.saturdayquizapp.repository.PrefsRepository
 import uk.co.mainwave.saturdayquizapp.repository.QuizRepository
 import uk.co.mainwave.saturdayquizapp.viewmodel.QuizViewModel
 
 val quizModule = module {
-    factory { QuizRepository(get()) }
-    factory { PrefsRepository(get()) }
-    viewModel { QuizViewModel(get(), get()) }
+    factoryOf(::QuizRepository)
+    factoryOf(::PrefsRepository)
+    viewModelOf(::QuizViewModel)
 }
