54fef7c0f1ef6af995f35af8a8c3e5829f19341a
Mark Whitaker <markw@bitrot.net>
2024-04-28 10:00:38 +0100

    Remove delaySeconds parameter in API call

--- app/src/main/java/uk/co/mainwave/saturdayquizapp/api/SaturdayQuizApi.kt
+++ app/src/main/java/uk/co/mainwave/saturdayquizapp/api/SaturdayQuizApi.kt
@@ -2,7 +2,6 @@ package uk.co.mainwave.saturdayquizapp.api
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
 import uk.co.mainwave.saturdayquizapp.model.Quiz
 
 interface SaturdayQuizApi {
@@ -10,8 +9,5 @@ interface SaturdayQuizApi {
     fun getLatestQuiz(
 //        @Query(value = "id")
 //        id: String = "lifeandstyle/2019/nov/16/quiz-tamara-in-a-green-bugatti-crow-red-legs-agatha-christie-thomas-eaton",
-
-        @Query(value = "delaySeconds")
-        delaySeconds: Int = 0
     ): Call<Quiz>
 }
