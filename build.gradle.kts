181ab03089567b7d5763c0fe998c53fe0c0de034
Mark Whitaker <mark@bitrot.net>
2024-10-18 06:49:46 +0100

    Upgrade Android Gradle Plugin to 8.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val gradleVersion = "8.7.0"
+    val gradleVersion = "8.7.1"
     val kotlinVersion = "2.0.21"
     repositories {
         google()
