83f08311441ea0a535d3143f74a2e93222f4c4ae
Mark Whitaker <mark@bitrot.net>
2024-12-10 16:31:00 +0000

    Remove commented-out id parameter in API

--- app/src/main/java/uk/co/mainwave/saturdayquizapp/api/SaturdayQuizApi.kt
+++ app/src/main/java/uk/co/mainwave/saturdayquizapp/api/SaturdayQuizApi.kt
@@ -6,8 +6,5 @@ import uk.co.mainwave.saturdayquizapp.model.Quiz
 
 interface SaturdayQuizApi {
     @GET("quiz")
-    fun getLatestQuiz(
-//        @Query(value = "id")
-//        id: String = "lifeandstyle/2019/nov/16/quiz-tamara-in-a-green-bugatti-crow-red-legs-agatha-christie-thomas-eaton",
-    ): Call<Quiz>
+    fun getLatestQuiz(): Call<Quiz>
 }
