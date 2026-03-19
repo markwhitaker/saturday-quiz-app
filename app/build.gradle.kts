0ba26a4be5553622e671ac7fb47274ee5ce3787e
Mark Whitaker <mark@bitrot.net>
2026-03-19 18:49:44 +0000

    Bump koin to 4.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 val junitVersion = "4.13.2"
-val koinVersion = "4.1.1"
+val koinVersion = "4.2.0"
 val kotlinCoroutinesVersion = "1.10.2"
 val kotlinVersion = "2.3.10"
 val lifecycleVersion = "2.10.0"
